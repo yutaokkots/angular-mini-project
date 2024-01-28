799895d004eff69b073de20583ae06e87935deee
yutaokkots <yuta.okkotsu@gmail.com>
2024-01-29 07:51:31 +0900

    Updated the to-do component for adding/deleting.

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm, FormsModule } from "@angular/forms"
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule ],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.scss'
 })
-export class ToDoComponent {
 
+// @NgModule({
+//   imports: [CommonModule]
+// })
+export class ToDoComponent implements OnInit {
+    taskArray = [{taskName: "go for a run", isCompleted: false}];
+
+    ngOnInit(): void {}
+
+    onSubmit(form: NgForm) {
+      console.log(form);
+  
+      this.taskArray.push({
+        taskName: form.controls['task'].value,
+        isCompleted: false
+      })
+  
+      form.reset();
+    }
+
+    onDelete(index: number) {
+      console.log(index);
+  
+      this.taskArray.splice(index, 1);
+    }
+  
+    onCheck(index: number){
+      console.log(this.taskArray);
+      this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+    }
 }
